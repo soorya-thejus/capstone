80813f6c15fe84739cd6d693d3908fd65080f670
VishnuCBalaraman <vishnucbalaram123@gmail.com>
2024-10-29 17:58:31 +0530

    updated organization model

--- Backend/Organization_MicroService/src/models/organization.ts
+++ Backend/Organization_MicroService/src/models/organization.ts
@@ -9,7 +9,7 @@ export interface IOrganization extends Document {
 
 const OrganizationSchema: Schema = new Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true,index: true,},
     type: { type: String, required: true },
     address: { type: String, required: true },
     contact_info: { type: String, required: true },
