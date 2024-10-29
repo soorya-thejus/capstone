c2685f3efb75028c3ae9fe1801d14ce32237af5d
VishnuCBalaraman <vishnucbalaram123@gmail.com>
2024-10-29 12:38:20 +0530

    updated accounts microservice

--- Backend/Accounts_MicroService/src/services/accountService.ts
+++ Backend/Accounts_MicroService/src/services/accountService.ts
@@ -1,4 +1,5 @@
 // src/services/AccountService.ts
+import axios from "axios";
 import { Account,IAccount } from "../models/Accounts";
 import { Types } from "mongoose";
 
@@ -20,5 +21,25 @@ export const updateAccountService = async (id: string, accountData: Partial<IAcc
 };
 
 export const deleteAccountService = async (id: string):Promise<IAccount|null> => {
+    const contacts = await getContactsByAccountId(id);
+
+    for (const contact of contacts) {
+        // Make an HTTP request to update the contact in the Contacts Microservice
+        await axios.patch(`http://localhost:5005/api/contacts/${contact._id}/remove-account`, { account_id: id });
+    }
     return await Account.findByIdAndDelete(id);
 };
+
+
+const CONTACT_MICROSERVICE_URL = 'http://localhost:5005/api/contacts';
+
+// Function to get contacts by account ID
+async function getContactsByAccountId(accountId: string) {
+    try {
+        const response = await axios.get(`${CONTACT_MICROSERVICE_URL}/accounts/${accountId}`);
+        return response.data; // Assuming it returns the contacts related to the account
+    } catch (error) {
+        console.error('Error fetching contacts:', error);
+        throw new Error('Unable to fetch contacts');
+    }
+}
