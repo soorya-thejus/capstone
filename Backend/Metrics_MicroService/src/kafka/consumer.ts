0edcf233d1abfd2cdb811a2d1a30b82f7ef75b0e
VishnuCBalaraman <vishnucbalaram123@gmail.com>
2024-11-05 22:36:39 +0530

    updated metrics

--- Backend/Metrics_MicroService/src/kafka/consumer.ts
+++ Backend/Metrics_MicroService/src/kafka/consumer.ts
@@ -24,7 +24,7 @@ export const runConsumer = async () => {
       if (topic === 'deal-events') {
         await updateMetricsFromDealEvent(eventData);
       } else if (topic === 'lead-events') {
-        await updateMetricsFromLeadEvent(eventData); // Handle lead event
+        await updateMetricsFromLeadEvent(eventData); 
       }
     },
   });
